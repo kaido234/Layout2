fae30f638e278e83edb6be06c3f36c08421cc188
kaido234 <dvbisip.student@ua.edu.ph>
2025-09-19 23:44:10 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,32 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      {/* Top Section */}
+      
       <div className="section top">
         <div className="top-box"></div>
+
         <div className="center">
           <div className="center-inner-container">
-            <div className="center-inner"></div>
-            <div className="center-inner"></div>
+            <div className="center-inner">Dave Vincent</div>
+            <div className="center-inner">Isip</div>
           </div>
         </div>
+
         <div className="top-box"></div>
       </div>
 
-      {/* Middle Section */}
+      
       <div className="section middle">
         <div className="mid-left"></div>
         <div className="mid-right"></div>
         <div className="mid-right"></div>
       </div>
 
-      {/* Bottom Section */}
+      
       <div className="section bottom">
         <div className="bottom-box purple"></div>
-        <div className="bottom-box"></div>
-        <div className="bottom-box"></div>
+        <div className="bottom-box">C-PCIT9</div>
+        <div className="bottom-box">IT3B</div>
         <div className="bottom-box purple"></div>
       </div>
     </div>
